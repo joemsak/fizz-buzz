26ff02e7b9b3d28ed9918bbec6b1f95d10a844cb
Joe Sak <joe@joesak.com>
2014-01-21 14:01:13 -0500

    Refactor -main to know very little, adding calculate-range, format calculate's cond

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,20 +1,21 @@
 (ns fizz-buzz.core)
-(declare calculate is-factor-of?)
+(declare calculate-range calculate is-factor-of?)
 
 (defn -main [number-range]
-  (let [fizz-buzzed (map #(calculate %) number-range)]
-    (apply println fizz-buzzed)))
+  (apply println (calculate-range number-range)))
+
+(defn calculate-range [number-range]
+  (map #(calculate %) number-range))
 
 (defn calculate [number]
   (let [fizz? (is-factor-of? number 3)
         buzz? (is-factor-of? number 5)
         both? (and fizz? buzz?)]
 
-    (cond
-      both? "fizzbuzz"
-      fizz? "fizz"
-      buzz? "buzz"
-      :else number)))
+    (cond both? "fizzbuzz"
+          fizz? "fizz"
+          buzz? "buzz"
+          :else number)))
 
 (defn is-factor-of? [n d]
   (zero? (rem n d)))
