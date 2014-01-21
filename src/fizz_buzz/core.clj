40080497416c15102684f2534a2497475a5f4858
Joe Sak <joe@joesak.com>
2014-01-21 13:49:33 -0500

    Rename is-divisible-by? to is-factor-of? and move it down below

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,11 +1,9 @@
 (ns fizz-buzz.core)
 
-(defn is-divisible-by? [n d]
-  (zero? (rem n d)))
 
 (defn calculate [number]
-  (let [fizz? (is-divisible-by? number 3)
-        buzz? (is-divisible-by? number 5)
+  (let [fizz? (is-factor-of? number 3)
+        buzz? (is-factor-of? number 5)
         both? (and fizz? buzz?)]
 
     (cond
@@ -17,3 +15,6 @@
 (defn run [number-range]
   (let [fizz-buzzed (map #(calculate %) number-range)]
     (apply println fizz-buzzed)))
+
+(defn is-factor-of? [n d]
+  (zero? (rem n d)))
